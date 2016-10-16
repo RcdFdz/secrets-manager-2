9bc69e9ee521ab16213062e3ee7857fb81b6d33e
Ricardo Fernandez <fernandez.domenech@gmail.com>
2016-10-16 19:34:35 +0200

    [ADD] Testing for secrets_manager class

--- secrets_manager.py
+++ secrets_manager.py
@@ -1,21 +1,40 @@
 #!/usr/local/bin/python3
 from gpg_tools import GPGTools
 
-json_content = {}
-
 class SecretsManager:
+	json_content = {}
+
 	def __init__(self, new_json = None):
-		gpg = GPGTools()
-		global json_content
 		if new_json or new_json == {}:
-			json_content = new_json
+			self.json_content = new_json
 		else:
-			json_content = gpg.decrypt_content()
+			gpg = GPGTools()
+			self.json_content = gpg.decrypt_content()
 
 	def delete_entry(self, id):
-		global json_content
-		json_content.pop(id)
-		return json_content
+		self.json_content.pop(id)
+		return self.json_content
+
+	def modify_values(self, id, list_values):
+		for key in list_values:
+			if key in self.json_content[id].keys():
+				self.json_content[id][key] = list_values[key]
+			else:
+				raise KeyError
+		return self.json_content
+
+	def modify_id(self, old_id, new_id):
+		aux = self.json_content[old_id]
+		self.json_content.pop(old_id)
+		self.json_content[new_id] = aux
+		return self.json_content
+
+	def add(self, id, list_values):
+		if id not in self.json_content:
+			self.json_content[id] = list_values
+		else:
+			raise KeyError
+		return self.json_content
 
 	def main(self):
 		pass
